3791018c6d7f8bcd2cfaf3e204463432ceee6bb6
Patricio Alejandro Pérez Valverde <pperez@boaboa.org>
2013-08-09 22:16:54 -0400

    Implementada seleccion de mochila en poblacion con ruleta

--- poblacion.py
+++ poblacion.py
@@ -1,17 +1,36 @@
+# -*- coding: utf-8 -*-
 '''
 Created on 09/08/2013
 
 @author: pperez
 '''
 
+from mochila import Mochila
+
 class Poblacion(object):
-    '''
-    classdocs
-    '''
-
-
-    def __init__(self, params):
-        '''
-        Constructor
-        '''
+    # Iniciamos la poblacion aleatoriamente
+    def __init__(self, productos, rnd, capacidad, n = 50):
+        self.container = []
+        self.rand = rnd
         
+        for i in range(n):
+            m = Mochila(i, productos, capacidad, rnd)
+            m.aleatorizar()
+            self.container.append(m)
+        self.container.sort(key = lambda mochila: mochila.beneficio, reverse = True)
+    
+    # Esto es para calculos de la ruleta
+    def totalBeneficio(self):
+        return sum(m.beneficio for m in self.container)
+    
+    # La famosa ruleta ...
+    def ruleta(self):
+        total = self.totalBeneficio()
+        dado = self.rand.random()
+        
+        suma = 0
+        for mochila in self.container:
+            suma += mochila.beneficio / total
+            if suma >= dado: # Llegamos al wilson, the chosen one
+                break
+        return mochila # Retornamos la mochila elegida al azar
